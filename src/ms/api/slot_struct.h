60f2c6d29d8895380ce4efbcf28c8d6799f36ad4
Charly Lamothe <charly.lamothe@sfr.fr>
2018-06-16 00:38:29 +0200

    - Fix licence preambles of all sources. - Fix Windows if/else in CMakeList.txt.

--- src/ms/api/slot_struct.h
+++ src/ms/api/slot_struct.h
@@ -14,7 +14,7 @@
 *   GNU General Public License for more details.                              *
 *                                                                             *
 *   You should have received a copy of the GNU General Public License         *
-*   along with UnknownEchoLib.  If not, see <http://www.gnu.org/licenses/>.   *
+*   along with LibMemorySlot.  If not, see <http://www.gnu.org/licenses/>.    *
 *******************************************************************************/
 
 #ifndef LIBMEMORYSLOT_SLOT_STRUCT_H
